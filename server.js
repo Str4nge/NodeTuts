928ba0c1867e02a05c82eaa8fdce31fa1f165a85
drstrange <code.amines@gmail.com>
2021-04-08 23:43:57 +0530

    Only View Engine and static file

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose'); //used database management
-const Blog = require('./models/blog');
 
 //express app
 const app = express();
 
-//connecting to MongoDB atlas connection string
-const dbURI = 'mongodb+srv://kyodaina:vishal@nodetuts.5vqll.mongodb.net/Node-tuts?retryWrites=true&w=majority';
-mongoose.connect(dbURI,{ useNewUrlParser: true, useUnifiedTopology:true } ) //then part will run after the async func is completed
-.then((result) => {
-    console.log('connected to database');               //here we are listening after the connection to the database is made.
-    app.listen(4000, () => {
-        console.log('Server started at 4000');
-    });
-}).catch((err) => console.log(err)) ;
+app.listen(4000, () => {
+    console.log('Server started at 4000');
+});
 
 //registering view engine
 app.set('view engine', 'ejs');
@@ -39,26 +31,19 @@ app.use((req, res, next) => {
 //Here we are just rendering the view inside Get
 app.get('/', (req, res) =>{
     //sending data objects along with rendering.
-    // const blogs = [
-    //     {title:'who was been messing up everything?', body:'It`s been AGATHA all along'},
-    //     {title:'who was been pulling every evil string?', body:'It`s been AGATHA all along'},
-    //     {title:'It`s too late to fix anything', body:'Now that everything has gone wrong'},
-    //     ];
+    const blogs = [
+        {title:'who was been messing up everything?', body:'It`s been AGATHA all along'},
+        {title:'who was been pulling every evil string?', body:'It`s been AGATHA all along'},
+        {title:'It`s too late to fix anything', body:'Now that everything has gone wrong'},
+        ];
 
-    // res.render('index',{title:"Home Page", blogs:blogs});
-    res.redirect('/blogs');
+    res.render('index',{title:"Home Page", blogs:blogs});
 });
 
 app.get('/about', (req, res) => {
     res.render('about',{title:"About Page"});
 });
 
-//finding the data from the database and sending to the view 
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({createdAt:-1})
-    .then((result) => res.render('index',{title: 'All Blogs', blogs: result}) )
-    .catch((err) => console.log(err));
-});
  
 app.get('/blogs/create', (req, res) => {
     res.render('createBlog',{title:"Create New Page"});
